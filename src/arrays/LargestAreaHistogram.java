b7bcce7179db39eafc2be0e04c6f63ea7594df4e
shubham sinha <shubham.si@media.net>
2022-04-28 23:44:22 +0530

    files update

--- src/arrays/LargestAreaHistogram.java
+++ src/arrays/LargestAreaHistogram.java
@@ -6,28 +6,33 @@ public class LargestAreaHistogram {
     public static int largestRectangleArea(int[] arr) {
         Stack<Integer> stk = new Stack<>();
         int maxArea = 0;
-        // smaller element to the left
-        stk.push(-1);
-        for (int i = 0; i< arr.length; i++ ) {
-            while(!(stk.peek() == -1) && arr[i] <= arr[stk.peek()]) {
-                int height = arr[stk.peek()];
-                stk.pop();
-                // next smaller heights to left and right
-                maxArea = Math.max(maxArea, (i - stk.peek() - 1)*height);
+        for(int i = 0; i< arr.length; i++ ) {
+            while(!stk.empty() && arr[i] <= arr[stk.peek()]) {
+                int height = arr[stk.pop()];
+
+                if (stk.empty()) {
+                    maxArea = Math.max(maxArea, i * height);
+                } else {
+                    // next smaller heights to left and right
+                    maxArea = Math.max(maxArea, (i - stk.peek() - 1)*height);
+                }
             }
             stk.push(i);
         }
+        while(!stk.empty()) {
+            int height = arr[stk.pop()];
 
-        while(!(stk.peek() == -1)) {
-            int height = arr[stk.peek()];
-            stk.pop();
-            // next smaller heights to left and right
-            maxArea = Math.max(maxArea, (arr.length - stk.peek() - 1) * height);
+            if (stk.empty()) {
+                maxArea = Math.max(maxArea, (arr.length)*height);
+            } else {
+                // next smaller heights to left and right
+                maxArea = Math.max(maxArea, (arr.length - stk.peek() - 1)*height);
+            }
         }
         return maxArea;
     }
 
     public static void main(String ...args) {
-        System.out.println(largestRectangleArea(new int[]{2}));
+        System.out.println(largestRectangleArea(new int[]{4,2}));
     }
 }
