b3a26927b9ebdd031edb28628fd22f9356e9be81
shubham sinha <shubham.si@media.net>
2022-05-02 13:35:26 +0530

    files update

--- src/trees/DiameterBTree.java
+++ src/trees/DiameterBTree.java
@@ -1,12 +1,12 @@
 package trees;
 
+// Diameter is the number of branches between 2 leaf nodes.
 public class DiameterBTree {
 	static class Node 
 	{  
 	    int data;  
 	    Node left, right;  
 	}
-	static int ans = Integer.MIN_VALUE;
 	static class A 
 	{ 
 	    int ans = Integer.MIN_VALUE; 
@@ -23,20 +23,19 @@ public class DiameterBTree {
 	
 	public static void main(String ...args) {
 		Node root = newNode(1);  
-	    root.left = newNode(2);  
+	    root.left = newNode(2);
 	    root.right = newNode(3);
-	    root.left.left = newNode(4);
-	    root.left.right = newNode(5);
-		root.right = newNode(3);
-//	    root.left.left.left = newNode(4);
-//	    root.left.left.right = newNode(5);
-//	    root.left.left.right.left = newNode(6);
-//	    root.left.left.right.left.right = newNode(7);
-	    
-//	    root.left.right.right = newNode(9);
-//	    root.left.right.right.left = newNode(10);
-//	    root.left.right.right.right = newNode(11);
-//	    root.left.right.right.right.left = newNode(12);
+	     root.left.left = newNode(4);
+	     root.left.right = newNode(5);
+	    root.left.left.left = newNode(4);
+	    root.left.left.right = newNode(5);
+	    root.left.left.right.left = newNode(6);
+	    root.left.left.right.left.right = newNode(7);
+
+	    root.left.right.right = newNode(9);
+	    root.left.right.right.left = newNode(10);
+	    root.left.right.right.right = newNode(11);
+	    root.left.right.right.right.left = newNode(12);
 
 	    System.out.println("Diameter is " + diameter(root));  
 	}
@@ -58,11 +57,12 @@ public class DiameterBTree {
 	  
 	    int left_height = height(root.left, ans);   
 	    int right_height = height(root.right, ans);  
-  
-	    ans.ans = Math.max(ans.ans, 									// max diameter so far
-	    		Math.max(1 + left_height + right_height, 				// either via-root or
-	                    1 + Math.max(left_height, right_height)));  	// if either path(l|r) is maximum
-	  
-	    return 1 + Math.max(left_height, right_height);  				// either max height including this node
+
+	    // for leaf to leaf don't consider max(l,r) + node(1) > as it can has any-path max(result)  > wrong answer
+		// either_path as max can give me any path max resulting in wrong answer
+	    ans.ans = Math.max(ans.ans, 							// max diameter so far
+				1 + left_height + right_height); 				// either via-root or
+
+	    return 1 + Math.max(left_height, right_height);  	    // max either path height including this node
 	} 
 }
--- src/trees/MaxSumPath.java
+++ src/trees/MaxSumPath.java
@@ -16,28 +16,48 @@ class MaxSumPath
             left = null;
             right = null;
         }
+
+        Node setTree(Node root){
+            Node temp = new Node(0);
+            //if tree is left most
+            if(root.right == null){
+                root.right=temp;
+            }
+            if (root.left == null){    //if tree is right most
+                root.left = temp;
+            }
+            return root;
+        }
     }
 
-    int maxPathSumAnyNode(Node node) {
+    static void maxSumPathNode(Node root) {
         /*
             max(
-                1) only node
+                1) only curr_node
                 2) via root (l + root + r)
-                3) either l,r => ( l or r) + node
+                3) either l,r => ( l or r) + root
             )
         */
         Result res = new Result();
-        maxSumBetweenAnyNodes(node, res);
+        maxSumBetweenAnyNodes(root, res);
+        System.out.println("between any nodes " + res.res);
+
+        // leaf to leaf
+        // adjust if root is either left rooted or right rooted only
+        // for gfg acceptance
+
+        if (root.left == null || root.right == null) {
+            root = root.setTree(root);
+        }
 
         Result leaf = new Result();
-        maxSumBetweenLeafNodes(node, leaf);
-
-        return res.res;
+        maxSumBetweenLeafNodes(root, leaf);
+        System.out.println("between leaf nodes "+ leaf.res);
     }
 
     // post -order approach (like diameter of a tree)
     // maximum possible sum b/w any nodes.
-    int maxSumBetweenAnyNodes(Node node, Result res) {
+    static int maxSumBetweenAnyNodes(Node node, Result res) {
         if (node == null) {
             return 0;
         }
@@ -56,23 +76,56 @@ class MaxSumPath
         return max_l_or_r;
     }
 
-    // maximum possible sum from one leaf node to another.
-    int maxSumBetweenLeafNodes(Node node, Result res) {
+    // maximum possible sum from one leaf node to another, (w/o including curr_node.data in max computation as separate)
+    static int maxSumBetweenLeafNodes(Node node, Result res) {
         if (node == null) {
             return 0;
         }
 
-        int l = maxSumBetweenAnyNodes(node.left, res);
-        int r = maxSumBetweenAnyNodes(node.right, res);
+        int l = maxSumBetweenLeafNodes(node.left, res);
+        int r = maxSumBetweenLeafNodes(node.right, res);
 
-        int max_l_or_r = Math.max(node.data,     // only node
-                Math.max(l , r) + node.data);    // either path
+        // will not consider either_path(max_l_or_r) for computing maximum result
+        // as this can give me any-node path max resulting in wrong answer
+        // either_path as max can give me any one leaf node path thus can't be added to calc. res_max
 
-        int temp = Math.max((l + r + node.data),     // via root
-                max_l_or_r);                        // either path
+        // update result only when considering leaf to leaf i.e., via root
+        if (node.left != null && node.right != null) {
+            res.res = Math.max(res.res, l + r + node.data);     // via - root leaf to leaf
+        }
 
-        res.res = Math.max(res.res, temp);
+        // either path
+        return (node.left == null) ? (r + node.data) : (l + node.data);
+    }
 
-        return max_l_or_r;
+    public static void main(String args[]) {
+        Node root = new Node(5);
+        // root.left = new Node(6);
+        root.right = new Node(6);
+//        root.left.left = new Node(-8);
+//        root.left.right = new Node(1);
+//        root.left.left.left = new Node(2);
+//        root.left.left.right = new Node(6);
+          root.right.left = new Node(-5);
+          root.right.right = new Node(5);
+//        root.right.right.right = new Node(0);
+//        root.right.right.right.left = new Node(4);
+//        root.right.right.right.right = new Node(-1);
+//        root.right.right.right.right.left = new Node(10);
+
+        maxSumPathNode(root);
     }
 }
+
+/*
+
+//--- for test case ---
+   Node root = new Node(-9);
+        root.left = new Node(6);
+        root.right = new Node(-10);
+       //         7
+      //        /    \
+        //    Null   -3
+      //     (case - 1)
+      //   value of res will be INT_MIN but the answer is 4 , which is returned by the
+ */
