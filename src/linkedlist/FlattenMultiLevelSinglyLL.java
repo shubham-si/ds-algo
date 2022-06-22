c45ef6b5231298fe145b33952b222815ceda4bec
shubham.si <sinhashubham07@gmail.com>
2022-06-22 22:23:35 +0530

    Updates

--- src/linkedlist/FlattenMultiLevelSinglyLL.java
+++ src/linkedlist/FlattenMultiLevelSinglyLL.java
@@ -6,13 +6,13 @@ import java.util.Queue;
 public class FlattenMultiLevelSinglyLL {
     /*
         You need to flatten the list in way that all nodes at the first level should come first, then nodes of second level,
-        1 - 2 - 3 - 4 - 5
-        |
-        6 - 7 - 8   9 - 10
-            |   |   |
-            11  12  13 - 14
-                |   |
-                15  16 - 17
+        1 - 2 - 3 -  4 - 5
+        |            |
+        6 - 7 - 8    9 - 10
+            |   |    |
+            11  12 - 13 - 14
+                |    |
+                15   16 - 17
 
         O/P ==> [1,2,3,4,5, 6,7,8,9,10, 11,12,13,14, 15,16,17]
      */
