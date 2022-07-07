c078cc6935581088cca0ebf3448b85d69716e779
shubham.si <sinhashubham07@gmail.com>
2022-07-07 22:44:19 +0530

    Updates

---
+++ src/dp/PallindromePartition.java
@@ -0,0 +1,59 @@
+package dp;
+
+public class PallindromePartition {
+
+    boolean isPallindrome(int i ,int j, String s) {
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int minPartition(int i, int n, String s, int[] dp) {
+        if (i == n) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int minCut = Integer.MAX_VALUE;
+
+        for (int j = i ; j < n; j++) {
+            if (isPallindrome(i,j,s)) {
+                int cost = 1 + minPartition(j + 1, n, s, dp);
+                minCut = Math.min(minCut, cost);
+            }
+        }
+
+        return dp[i] = minCut;
+    }
+
+    // tabulation
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        dp[n] = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            int minCut = Integer.MAX_VALUE;
+
+            for (int j = i ; j < n; j++) {
+
+                if (isPallindrome(i,j,s)) {
+                    int cost = 1 + dp[j + 1];
+                    minCut = Math.min(minCut, cost);
+                }
+            }
+            dp[i] = minCut;
+        }
+
+        return dp[0] - 1;
+    }
+}
