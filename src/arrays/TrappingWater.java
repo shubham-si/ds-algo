1336c45380326cb7d858ae86c23f152168d3220d
shubham.si <shubham.si@media.net>
2024-06-11 23:46:54 +0530

    updates

--- src/arrays/TrappingWater.java
+++ src/arrays/TrappingWater.java
@@ -1,7 +1,11 @@
 package arrays;
 
+import java.util.Stack;
+
 public class TrappingWater {
-    static long trappingWater(int arr[], int n) {
+
+    // O(n), O(n)
+    static long trappingWaterPrePostArray(int arr[], int n) {
         int lh[] = new int[n];
         int rh[] = new int[n];
 
@@ -20,5 +24,59 @@ public class TrappingWater {
             count += Math.min(lh[i], rh[i]) - arr[i];
         }
         return count;
+    }
+
+    // O(n), O(n)
+    static long trappingWaterUsingStack(int arr[], int n) {
+        int res = 0, l = 0;
+        // stack of lower heights
+        Stack<Integer> stack = new Stack();
+
+        while(l < n) {
+            while (!stack.empty() && arr[l] > arr[stack.peek()]) {
+                int topTower = arr[stack.pop()];
+                if (stack.empty()) {
+                    // nothing to compare
+                    break;
+                }
+                int w = (l - 1) - stack.peek();
+                int h = Math.min(arr[stack.peek()], arr[l]) - topTower;
+                res += w * h;
+            }
+            stack.push(l);
+            l++;
+        }
+        return res;
+    }
+
+    // r:O(n), s:O(1)
+    static long trappingWaterTwoPointer(int arr[], int n) {
+        int res = 0, l = 0, r = n - 1;
+        int lmax = 0 ,rmax = 0; // between these
+        while (l < r) {
+            if (arr[l] < arr[r]) {
+                if (lmax < arr[l]) {
+                    lmax = arr[l];
+                } else {
+                    res = res + (lmax - arr[l]);
+                }
+                l++;
+            } else {
+                if (rmax < arr[r]) {
+                    rmax = arr[r];
+                } else {
+                    res = res + (rmax - arr[r]);
+                }
+                r--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int []arr = {4,2,3,5,6}; // {3,2,2,3,1,4,3,2,4,2,6};
+        System.out.println(TrappingWater.trappingWaterTwoPointer(arr, arr.length));
+        System.out.println(TrappingWater.trappingWaterUsingStack(arr, arr.length));
+        System.out.println(TrappingWater.trappingWaterPrePostArray(arr, arr.length));
     }
 }
--- src/numberTheory/PrimeFactorization.java
+++ src/numberTheory/PrimeFactorization.java
@@ -14,7 +14,7 @@ public class PrimeFactorization {
     // O(logn)
     public static void factorizationWithoutSieve(int n) {
         Map<Integer, Integer> pf = new HashMap();
-        for (int i = 2; i*i <= n ; i++) {
+        for (int i = 2; i * i <= n ; i++) {
             while (n % i == 0) {
                 pf.put(i, 1 + pf.getOrDefault(i, 0));
                 n = n / i;
