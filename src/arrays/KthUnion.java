6e7196d57b8d2aca168a72e4e7ccd78909af8ce7
shubham.si <shubham.si@media.net>
2024-06-16 00:25:15 +0530

    updates

--- src/arrays/KthUnion.java
+++ src/arrays/KthUnion.java
@@ -5,40 +5,40 @@ import java.util.Arrays;
 // expected complexity log(k)
 
 public class KthUnion {
-	
+
 	// considering minimum(A.length) always --> lenA < lenB
 	static int KthRecursive(int A[], int lenA, int B[], int lenB, int k) {
-		
+
 		if (((A.length + B.length) < k) || k < 1) {
 			return -1;
 		}
-		
+
 		// considering first array in the argument has minimum(<=) length
 		if (lenA > lenB) {
 			return KthRecursive(B,lenB,A,lenA,k);
 		}
-		
+
 		// len(A) is 0 then (k-1)th from B is the kth smallest
 		if (lenA == 0) {
 			return B[k-1];
 		}
-		
+
 		// if only one element to process then min(0th from both arrays) is the kth smallest
 		if (k == 1) {
 			return Math.min(A[0], B[0]);
 		}
-		
+
 		// if lenA + lenB is the kth then max(nth from both arrays) is the kth smallest as it comes at the last
 		if ((lenA + lenB) == k) {
 			return Math.max(A[lenA-1], B[lenB-1]);
 		}
-		
+
 		// taking invariant as (i+j <= k)
 		int i = Math.min(lenA, k/2);
 		int j = Math.min(lenB, k/2);	// or (j = Math.min(lenB, k-i))
-		
+
 		// number of elements skipped from A or B based on condition
-		
+
 		if (A[i-1] < B[j-1]) {
 			// then 0...i-1 elements can be skipped since kth will not lie in this range
 			int temp[] = Arrays.copyOfRange(A, i, lenA);
@@ -78,10 +78,12 @@ public class KthUnion {
 		}
 		return ele;
 	}
-	
+
 	public static void main(String...strings) {
-		int A[] = {1,12,13,84};
-		int B[] = {0,2,88};
+		int A[] = {1,5,6};
+		int B[] = {2,3,4};
+		System.out.println(KthRecursive(A,A.length,B,B.length,4));
+		System.out.println(KthIterative(A,B,4));
 		A = new int[]{5 ,33 ,55 ,65 ,76 ,80 ,90};
 		B = new int[]{10 ,13 ,14 ,15 ,15 ,22 ,27 ,32 ,34 ,36 ,36 ,37 ,39 ,40 ,42 ,45 ,49 ,50 ,50 ,53 ,56 ,56 ,57 ,61 ,65 ,66 ,70 ,70 ,71 ,74 ,78 ,84 ,87 ,90 ,91 ,94 ,94, 96, 99};
 
