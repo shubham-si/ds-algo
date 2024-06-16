c1180829978baeb078ea7712929131689ee7cd42
shubham.si <shubham.si@media.net>
2024-06-16 14:22:44 +0530

    updates

--- src/arrays/SearchRotatedArray.java
+++ src/arrays/SearchRotatedArray.java
@@ -5,7 +5,7 @@ package arrays;
 
 public class SearchRotatedArray {
     // non duplicates
-    int search(int arr[], int l, int h, int key)
+    int withNoDuplicates(int arr[], int l, int h, int key)
     {
         int st = 0, e = arr.length - 1, mid;
 
@@ -39,4 +39,46 @@ public class SearchRotatedArray {
     }
 
     // with duplicates
+    public int withDuplicates(int[] arr, int k) {
+        int s = 0 , e = arr.length - 1;
+
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (arr[m] == k) {
+                return m;
+            }
+
+            // edge case:
+            // arr[start] == arr[mid] could be possible
+            // the first half could be out of order (i.e. not in the ascending order)
+            // eg., [3, 1, 2, 3, 3, 3, 3]
+            // In that case, it is guaranteed that arr[high] also equal to arr[mid] > arr[mid] == arr[low] == arr[high]
+            // adjust search space by updating positions by 1
+            if (arr[s] == arr[m] && arr[e] == arr[m]) {
+                s++;
+                e--;
+                continue;
+            }
+
+            // decide where to go
+
+            if(arr[s] <= arr[m]) {
+                // left part is sorted
+                if (k >= arr[s] && k < arr[m]) {
+                    e = m - 1;
+                } else {
+                    s = m + 1;
+                }
+            } else {
+                if (k > arr[m] && k <= arr[e]) {
+                    s = m + 1;
+                } else {
+                    e = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
